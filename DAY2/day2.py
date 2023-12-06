4f557e3a8e05928f7e55b8e1a4eee8581c62b0d0
Poutrozaure <79838641+Poutrozaure@users.noreply.github.com>
2023-12-06 23:47:11 +0100

    Update day2.py

--- DAY2/day2.py
+++ DAY2/day2.py
@@ -1,38 +1,55 @@
 
-input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+with open('dataset.txt') as f:
+    inputs=f.readlines()
+    
+#input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
 
 colors = ["red","green","blue"]
 red_max = 12
-red_show =0
 green_max = 13
-green_show =0
 blue_max=14
-blue_show = 0
 result = 0
 
-#find max showned of each colors
-
-#preprocess data
-game_trial = ord(input[5])-48
-test = input.split(";")
-test[0]=test[0][7:]
-
-for game,throw in enumerate(test):
-        index = throw.find(colors[0])
-        value = ord(throw[index-2])-48
-        if index != -1 and value>red_show:
-            red_show = throw[index-2]
-            
-        index = input.find(colors[1])
-        value = ord(throw[index-2])-48
-        if index != -1 and value>green_show:
-            green_show = throw[index-2]
-            
-        index = input.find(colors[2])
-        value = ord(throw[index-2])-48
-        if index != -1 and value>blue_show:
-            blue_show = throw[index-2]
-
-print(red_show,green_show,blue_show)
-#if(red_show<=red_max and green_show<= green_max and blue_show <= blue_max):
-#    result +=1
+for input in inputs:
+    #print(input)
+    red_show =0
+    green_show =0
+    blue_show = 0
+    #find max showned of each colors
+    
+    #preprocess data
+    test = input.split(";")
+    index_1 = test[0].find(" ")
+    index_2 = test[0].find(":")
+    game_trial = int(input[index_1+1:index_2])
+    test[0]=test[0][index_2+1:]
+    print(test)
+    for game,throw in enumerate(test):
+            index = throw.find(colors[0])
+            #print(index)
+            if index!=-1 : 
+                value = int(throw[index-3:index-1])
+                if value>red_show:
+                    red_show = value
+                
+            index = throw.find(colors[1])
+            #print(index)
+            if index!=-1 : 
+                value = int(throw[index-3:index-1])
+                if value>green_show:
+                    green_show = value
+                
+            index = throw.find(colors[2])
+            #print(index)
+            if index!=-1 :
+                value = int(throw[index-3:index-1])
+                if value>blue_show:
+                    blue_show = value
+    
+    
+    print(red_show,green_show,blue_show)
+    
+    if(red_show<=red_max and green_show<= green_max and blue_show <= blue_max):
+        result +=game_trial
+        
+print(result)
